a66a9e26c00586a9ae6679c477dcd2ea3ef6b92b
xiaobocs <xiaobocs@gmail.com>
2023-02-20 23:30:48 +0800

    createpage of blog and enable css

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,2 +1,13 @@
+// custom typefaces
+//import "@fontsource/montserrat/variable.css"
+//import "@fontsource/merriweather"
+// normalize CSS across browsers
+import "./src/normalize.css"
+// custom CSS styles
+import "./src/style.css"
+
+// Highlighting for code blocks
+import "prismjs/themes/prism.css"
+
 import "./src/styles/global.css";
 import "katex/dist/katex.min.css";
--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,15 @@
 module.exports = {
   siteMetadata: {
     title: `mdx attempt`,
-    siteUrl: `https://www.yourdomain.tld`,
+    author: {
+      name: `Kyle Mathews`,
+      summary: `who lives and works in San Francisco building useful things.`,
+    },
+    description: `A starter blog demonstrating what Gatsby can do.`,
+    siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
+    social: {
+      twitter: `kylemathews`,
+    },
   },
   plugins: [
     "gatsby-plugin-mdx",
